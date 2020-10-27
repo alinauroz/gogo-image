9bb659995e1f0585094e728c168bad363b5f68a7
MacBook <alinauroze@hotmail.com>
2020-10-27 23:49:11 +0500

    feat: added logsign to route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,11 @@ import TopBar from './Components/TopBar'
 import About from './Components/pages/About'
 import WhyUs from './Components/pages/WhyUs'
 import Login from './Components/pages/LogSign'
+import Terms from './Components/pages/Terms'
+
+function Signup () {
+  return <Login selected={1} />
+}
 
 function App() {
   return (
@@ -16,10 +21,12 @@ function App() {
           <Route>
             <Route path="/about" component={About} />
             <Route path="/why-us" component={WhyUs} />
+            <Route path="/terms" component={Terms} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
           </Route>
         </Switch>
       </main>
-      <Login />
     </>
   );
 }
