8be07fb8ee3dd73717988dbde446292053007665
MacBook <alinauroze@hotmail.com>
2020-10-28 01:12:29 +0500

    feat: added footer links

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import {Text, Image, View} from './Basic/AppComponents'
+import { Link } from 'react-router-dom'
 
 import gmail from '../data/icons/gmail.png'
 import fb from '../data/icons/fb.png'
@@ -30,6 +31,14 @@ export default function () {
                     source = {mail}
                     style = {{width: 40, margin: 5}}
                 />
+            </View>
+            <View style = {{display: 'inline-block', marginTop: 20}}>
+                <Link to = '/about' className = 'topbar-links' style = {{color: 'white'}}>About</Link>
+                <Link to = '/why-us' className = 'topbar-links' style = {{color: 'white'}}>Why Us</Link>
+                <Link to = '/faq' className = 'topbar-links' style = {{color: 'white'}}>FAQ</Link>
+                <Link to = '/contact' className = 'topbar-links' style = {{color: 'white'}}>Contact</Link>
+                <Link to = '/terms' className = 'topbar-links' style = {{color: 'white'}}>Terms</Link>
+                <Link to = '/privacy' className = 'topbar-links' style = {{color: 'white'}}>Privacy</Link>
             </View>
         </footer>
     )
