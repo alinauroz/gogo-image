67bcd191433da234ea819bd37c84331a08c3a46c
MacBook <alinauroze@hotmail.com>
2020-10-27 22:45:09 +0500

    style: input field style added

--- client/src/Components/pages/LogSign.jsx
+++ client/src/Components/pages/LogSign.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import Box from '../Container/Box'
 import {View, Text, Button} from '../Basic/AppComponents'
+import FormRenderer from '../../utils/FormRenderer'
+
+const loginFields = [
+    {title: 'Email', name: 'email', type: 'email', required: true},
+    {title: 'Password', name: 'password', type: 'password', required: true}
+]
+
+const signupFields = [
+
+]
 
 export default function (props) {
 
@@ -19,6 +29,17 @@ export default function (props) {
                     className = {selected == 1 ? 'pair-button pair-button-selected': "pair-button"}
                     onPress = {() => setSelected(1)}
                 />
+                <br /><br />
+                {
+                    selected ? 
+                    <FormRenderer 
+                        fields = {signupFields}
+                    />
+                    : 
+                    <FormRenderer 
+                        fields = {loginFields}
+                    />
+                }
             </View>
         </Box>
     )
