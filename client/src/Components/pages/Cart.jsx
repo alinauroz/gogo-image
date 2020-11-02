0a6eb2078939df3840117b953bcae6fa3c8aaa2e
MacBook <alinauroze@hotmail.com>
2020-11-02 15:42:42 +0500

    style: added new

--- client/src/Components/pages/Cart.jsx
+++ client/src/Components/pages/Cart.jsx
@@ -18,31 +18,31 @@ export default function () {
         <View style = {{display: 'block', marginTop: 50, width: '100%', marginLeft: 'calc(0% - 0px)'}}>
             <Table style = {{width: '100%'}}>
                 <Tr className = 'cart-tr'>
-                    <Td className = 'cart-td'>
+                    <Td className = 'cart-td cart-th'>
                         <Text style = {{fontWeight: 'bold', fontSize: 13}}></Text>
                     </Td>
-                    <Td className = 'cart-td'>
+                    <Td className = 'cart-td cart-th'>
                         <Text style = {{fontWeight: 'bold', fontSize: 13}}>Kaboom Template</Text>
                     </Td>
-                    <Td className = 'cart-td'>
+                    <Td className = 'cart-td cart-th'>
                         <Text style = {{fontWeight: 'bold', fontSize: 13}}>Service Type</Text>
                     </Td>
-                    <Td className = 'cart-td'>
+                    <Td className = 'cart-td cart-th'>
                         <Text style = {{fontWeight: 'bold', fontSize: 13}}>Your Image</Text>
                     </Td>
-                    <Td className = 'cart-td'>
+                    <Td className = 'cart-td cart-th'>
                         <Text style = {{fontWeight: 'bold', fontSize: 13}}>Superimpose # 1</Text>
                     </Td>
-                    <Td className = 'cart-td'>
+                    <Td className = 'cart-td cart-th'>
                         <Text style = {{fontWeight: 'bold', fontSize: 13}}>Superimpose # 2</Text>
                     </Td>
-                    <Td className = 'cart-td'>
+                    <Td className = 'cart-td cart-th'>
                         <Text style = {{fontWeight: 'bold', fontSize: 13}}>Remove</Text>
                     </Td>
-                    <Td className = 'cart-td'>
+                    <Td className = 'cart-td cart-th'>
                         <Text style = {{fontWeight: 'bold', fontSize: 13}}>Total</Text>
                     </Td>
-                    <Td className = 'cart-td'>
+                    <Td className = 'cart-td cart-th'>
                         <Text style = {{fontWeight: 'bold', fontSize: 13}}></Text>
                     </Td>
                 </Tr>
