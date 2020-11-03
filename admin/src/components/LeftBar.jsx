7afe3a68a5ef55600629001fb353b7fe11ccfe07
MacBook <alinauroze@hotmail.com>
2020-11-03 18:32:27 +0500

    feat: left bar button renderer added

--- admin/src/components/LeftBar.jsx
+++ admin/src/components/LeftBar.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 export default function (props) {
     return (
         <div className = 'bar'>
-            
+            {
+                props.buttonList.map(button => {
+                    return (
+                        <button className = 'bar-button'>
+                            <span className = 'glyphicon glyphicon-link'></span>
+                            <span className = 'bar-button-title'>{button.title}</span>
+                        </button>
+                    )
+                })
+            }
         </div>
     )
 }
