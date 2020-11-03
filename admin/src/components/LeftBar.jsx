b1dc1aeeb67547f9c42b843c69187bf651017a4e
MacBook <alinauroze@hotmail.com>
2020-11-03 18:39:10 +0500

    feat: select button from leftbar

--- admin/src/components/LeftBar.jsx
+++ admin/src/components/LeftBar.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
 export default function (props) {
+
+    const [selected, setSelected] = React.useState();
+
+    const onClick = (e, index) => {
+        setSelected(index);
+    }
+
     return (
         <div className = 'bar'>
             {
-                props.buttonList.map(button => {
+                props.buttonList.map((button, index) => {
+                    if (index == selected)
+                        var class_ = 'bar-button-selected'
+
                     return (
-                        <button className = 'bar-button'>
+                        <button className = {'bar-button ' + class_} onClick = {(e) => onClick(e, index)}>
                             <span className = 'glyphicon glyphicon-link'></span>
                             <span className = 'bar-button-title'>{button.title}</span>
                         </button>
