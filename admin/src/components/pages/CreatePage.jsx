2909697f6d8d37ffd694a08a5f9c097483766f46
MacBook <alinauroze@hotmail.com>
2020-11-05 19:05:04 +0500

    feat: create page added

--- admin/src/components/pages/CreatePage.jsx
+++ admin/src/components/pages/CreatePage.jsx
@@ -3,21 +3,46 @@ import Field from '../unit/Field'
 import CKEditor from 'react-ckeditor-component'
 
 export default function () {
+
+    
+
     return (
         <div class = 'card'>
             <Field 
                 name = 'title'
-                placeholder = 'title'
+                placeholder = 'Title'
                 title = 'Title'
 
             />
             <br />
+            <span style = {{marginBottom: 10}}>
+
+            </span>
             <CKEditor 
                 activeClass="editor" 
                 content={""} 
                 onChange={() => {}}
                 style = {{marginTop: 10}}
             />
+            <br />
+            <Field 
+                title = 'URL'
+                name = 'url'
+                placeholder = 'URL'
+            />
+            <div style = {{marginTop: 15}}>
+                <input 
+                    type = 'button'
+                    value = 'Preview'
+                    className = 'btn btn-primary'
+                />
+                <input 
+                    type = 'button'
+                    value = 'Save'
+                    className = 'btn btn-primary'
+                    style = {{marginLeft: 10}}
+                />
+            </div>
         </div>
     )
 }
