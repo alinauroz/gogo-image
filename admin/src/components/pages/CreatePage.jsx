b87da598c874f84602101e156e2617883c50b704
MacBook <alinauroze@hotmail.com>
2020-11-05 19:20:30 +0500

    feat: addon added to field

--- admin/src/components/pages/CreatePage.jsx
+++ admin/src/components/pages/CreatePage.jsx
@@ -33,6 +33,7 @@ export default function () {
                 title = 'URL'
                 name = 'url'
                 placeholder = 'URL'
+                addon = '/'
             />
             <div style = {{marginTop: 15}}>
                 <input 
--- admin/src/components/unit/Field.jsx
+++ admin/src/components/unit/Field.jsx
@@ -3,6 +3,19 @@ export default function (props) {
     return (
         <div style = {props.style}>
             <p className = 'field-title'>{props.title}</p>
+            {
+            props.addon ?
+            <div class="input-group">
+                <span class="input-group-addon">{props.addon}</span>
+                <input 
+                    type = {props.inputType}
+                    placeholder = {props.placeholder || ""}
+                    name = {props.name}
+                    onChange = {props.onChange}
+                    className = 'form-control'
+                />
+            </div>
+            :
             <input 
                 type = {props.inputType}
                 placeholder = {props.placeholder || ""}
@@ -10,6 +23,7 @@ export default function (props) {
                 onChange = {props.onChange}
                 className = 'form-control'
             />
+            }
         </div>
     )
 }
