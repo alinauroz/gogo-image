945bb2073051ba4ab148a69668c3f23ff7ed98ed
MacBook <alinauroze@hotmail.com>
2020-11-07 00:37:34 +0500

    feat: renderer added

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import LeftBar from './components/LeftBar'
 import './App.css'
 
@@ -7,31 +8,39 @@ import CreatePage from './components/pages/CreatePage'
 import AddCoupon from './components/pages/AddCoupon'
 import AddFaq from './components/pages/AddFaq'
 
-const BUTTONS = Object.freeze([
-  {title: 'Home'},
-  {title: 'Super Admin'},
-  {title: 'Add Staff'},
-  {title: 'Users'},
-  {title: 'Manage Content'},
-  {title: 'Add Pages'},
-  {title: 'FAQs'},
-  {title: 'Add FAQs'},
-  {title: 'Price & Fee'},
-  {title: 'Manage Posting'},
-  {title: 'Sales Data'},
-  {title: 'Order History'},
-  {title: 'Products'},
-  {title: 'Add Product'}
-]);
+const Renderer = (props) => {
+  return props.el;
+}
 
 export default function () {
+
+  const [Screen, setScreen] = React.useState(AddStaff);
+
+  const BUTTONS = Object.freeze([
+    {title: 'Home'},
+    {title: 'Super Admin'},
+    {title: 'Add Staff'},
+    {title: 'Users'},
+    {title: 'Manage Content'},
+    {title: 'Add Pages'},
+    {title: 'FAQs'},
+    {title: 'Add FAQs'},
+    {title: 'Price & Fee'},
+    {title: 'Manage Posting'},
+    {title: 'Sales Data'},
+    {title: 'Order History'},
+    {title: 'Products'},
+    {title: 'Add Product'}
+  ]);
+
   return (
     <>
       <LeftBar 
         buttonList = {BUTTONS}
       />
       <div className = 'main-container'>
-        <AddFaq />
+        {console.log(Screen)}
+        <Renderer el = {Screen} />
       </div>
     </>
   )
