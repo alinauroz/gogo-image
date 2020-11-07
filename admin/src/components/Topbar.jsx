2da62ed9174b206a837999ca9826d2453dcc0d56
MacBook <alinauroze@hotmail.com>
2020-11-07 19:24:20 +0500

    feat: logout function added to topbar

--- admin/src/components/Topbar.jsx
+++ admin/src/components/Topbar.jsx
@@ -6,6 +6,21 @@ export default function () {
             <span>
                 <h3 style = {{display: 'inline-block', margin: 0, marginTop: 10, marginLeft: 20}}>Admin LTE</h3>
             </span>
+            <div className = 'topbar-user' data-toggle="collapse" href="#collapseExample">
+                Ali Nauroze ▾
+            </div>
+            <div class = 'collapse' id = 'collapseExample' style = {{zIndex: 2, position: 'absolute', width: 200, right: 0, top: 50, padding: 5, background: 'white', boxShadow: '0px 0px 2px grey'}}>
+                <input 
+                    value = 'Logout' 
+                    type = 'button' 
+                    className = 'btn btn-warning' 
+                    onClick = {() => {
+                        localStorage.setItem('token', '');
+                        localStorage.setItem('admin', '');
+                        window.location.reload();
+                    }}
+                />
+            </div>
         </div>
     )
 }
