e41e8c931778d08c1963119b01416e4cf84523cb
MacBook <alinauroze@hotmail.com>
2020-11-07 13:30:52 +0500

    feat: viewer added in view blogs

--- admin/src/components/pages/ViewBlogs.jsx
+++ admin/src/components/pages/ViewBlogs.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import {api} from '../../data/api'
+import Viewer from '../../utils/Viewer'
 
 export default function () {
 
     const [data, setData] = React.useState();
+
+    (async () => {
+        let res = await fetch(api + 'blogs');
+        let data_ = await res.json();
+        setData(data_);
+    })()
 
     return (
         <div className = 'card'>
             <h3 style = {{margin: 0, marginBottom: 10}}>View Blogs</h3>
             {
                 data ?
-                <></>
+                <Viewer data = {data.data} />
                 : "loading ..."
             }
         </div>
--- admin/src/data/api.js
+++ admin/src/data/api.js
@@ -1,6 +1,8 @@
-export const api = {
+export const apis = {
     dev: 'http://localhost:5000/',
     prod: '/'
 }
 
 export const mode = 'dev';
+
+export const api = apis[mode];
