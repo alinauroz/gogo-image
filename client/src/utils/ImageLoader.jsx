6398490994b41864b2b5cc638c4928941cdcd249
MacBook <alinauroze@hotmail.com>
2020-11-09 19:26:34 +0500

    feat: added containers for ImageLoader

---
+++ client/src/utils/ImageLoader.jsx
@@ -0,0 +1,16 @@
+import React from React
+import {Text, View, Image} from '../Components/Basic/AppComponents'
+
+export default function () {
+
+    const [images, setImages] = React.useState();
+
+    return (
+        <View style = {{}}>
+            <View style = {{flexDirection: 'row', justifyContent: 'center'}}>
+                A
+            </View>
+        </View>
+    )
+
+}
