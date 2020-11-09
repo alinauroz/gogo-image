917c6920943795189e42fd29ccfab8b311796ef9
MacBook <alinauroze@hotmail.com>
2020-11-09 20:22:03 +0500

    feat: compress and view thumb added

--- client/src/utils/ImageLoader.jsx
+++ client/src/utils/ImageLoader.jsx
@@ -20,7 +20,7 @@ const resize = async (file, options = {}) => {
     try {
         let compressedFile = await imageCompression(file, options_);
         let base64 = await getBase64(compressedFile);
-        console.log(base64)
+        return base64;
     }
     catch (err) {
         throw err;
@@ -29,29 +29,38 @@ const resize = async (file, options = {}) => {
 
 export default function (props) {
 
-    const [images, setImages] = React.useState([]);
+    const [images, setImages] = React.useState({});
+    const [toView, setToView] = React.useState();
 
-    const handleChange = (e) => {
+    const handleChange = async (e) => {
 
         let file = e.target.files[0];
 
         if (props.sizes) {
             for (let x in props.sizes) {
                 if (typeof props.sizes[x] === 'object') {
-                    resize(file, props.sizes[x]);
+                    let base64 = await resize(file, props.sizes[x]);
+                    setImages({
+                        ... images,
+                        [x]: base64
+                    })
                 }
                 else {
 
                 }
             }
         }
+
+        let thumb = await resize(file, {maxWidthOrHeight: 120});
+        setToView(thumb);
+
     }
 
     return (
         <View style = {{}}>
             <View style = {{flexDirection: 'row', justifyContent: 'center', textAlign: 'center', width: '100%'}}>
                 {
-                    images.length === 0 ?
+                    Object.keys(images).length === 0 ?
                     <View style = {{display: 'inline-block', verticalAlign: 'top', height: 25, width: '100%'}}>
                         <input onChange = {handleChange} type = 'file' style = {{width: '100%', height: '100%', margin: 0, opacity: 0}} />
                         <View style = {{overflow: 'hidden', width: '100%', height: '100%', marginTop: '-25px', zIndex: 2, textAlign: 'center'}}>
@@ -59,7 +68,9 @@ export default function (props) {
                         </View>
                     </View>
                     :
-                    <></>
+                    <>
+                        <img src = {toView} />
+                    </>
                 }
             </View>
         </View>
