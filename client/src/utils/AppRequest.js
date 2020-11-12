7e4749a63a8d3ab082878a005184323239946d4e
MacBook <alinauroze@hotmail.com>
2020-11-12 14:21:49 +0500

    feat: request util added

---
+++ client/src/utils/AppRequest.js
@@ -0,0 +1,28 @@
+import {api} from '../data/api'
+
+const getToken = localStorage.getItem('token')
+
+const toQuery = () => {
+
+}
+
+export const request = ({route, params, body, query, method}) => {
+
+    try {
+        
+        let res = await fetch(api + route + params + '?' + toQuery(query), {
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Cookie': `jwt=${getToken()}`
+            },
+            body: JSON.stringify(body)
+        });
+
+        return res.json();
+    }
+    catch (err) {
+        throw err;
+    }
+
+}
