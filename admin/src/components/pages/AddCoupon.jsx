b1503e762c1b4261880236e6702aac64f0b3119c
MacBook <alinauroze@hotmail.com>
2020-11-22 20:47:40 +0500

    feat: add coupon added

--- admin/src/components/pages/AddCoupon.jsx
+++ admin/src/components/pages/AddCoupon.jsx
@@ -2,14 +2,44 @@ import React from 'react'
 import Field from '../unit/Field'
 import Viewer from '../../utils/Viewer'
 import getFormData from '../../utils/getFormData'
+import {request} from '../../utils/request'
 
 export default function () {
 
     const [forAll, setForAll] = React.useState(true);
+    const [message, setMessage] = React.useState('');
 
-    const addCoupon = (e) => {
+    const addCoupon = async (e) => {
         e.preventDefault();
-        console.log(getFormData(e.target))
+
+        let formData = (getFormData(e.target));
+        formData.forAll = forAll;
+        
+        //formData.validFor = formData.validFor.split(' ').filter(tag => tag !== ' ' & tag !== '');
+
+        formData.startDate = new Date(formData.startDate).getTime();
+        formData.endDate = new Date(formData.endDate).getTime();
+        try {
+
+            let res = await request({
+                route: 'coupons',
+                method: 'POST',
+                credentials: 'include',
+                body: formData
+            })
+
+            if (res.status == 'success')
+                setMessage('Coupon added successfully');
+            else {
+                setMessage('Problem adding Coupon');
+            }
+
+        }
+        catch (err) {
+            setMessage('Error')
+            console.log(err)
+        }
+
     }
 
     return (
@@ -46,18 +76,20 @@ export default function () {
                 />
                 <Field 
                     name = 'discount'
-                    title = 'Start Date'
-                    placeholder = 'Start Date'
+                    title = 'Discount'
+                    placeholder = 'Discount'
                     inputType = 'number'
                     style = {{marginTop: 10}}
                 />
-                <Field 
-                    name = 'maxDiscount'
-                    title = 'Maximum Discount'
-                    placeholder = 'Maximum Discount'
-                    inputType = 'number'
-                    style = {{marginTop: 10}}
-                />
+                {
+                //<Field 
+                //    name = 'maxDiscount'
+                //    title = 'Maximum Discount'
+                //    placeholder = 'Maximum Discount'
+                //    inputType = 'number'
+                //    style = {{marginTop: 10}}
+                ///>
+                }
                 <Field 
                     name = 'startDate'
                     title = 'Start Date'
@@ -72,14 +104,24 @@ export default function () {
                     inputType = 'date'
                     style = {{marginTop: 10}}
                 />
-                <Field 
-                    name = 'validFor'
-                    title = 'Type of Use'
-                    placeholder = 'Separate tags using spaces'
-                    inputType = 'text'
-                    style = {{marginTop: 10}}
-                    disabled = {forAll}
-                />
+                {
+                //<Field 
+                //    name = 'validFor'
+                //    title = 'Type of Use'
+                //    placeholder = 'Separate tags using spaces'
+                //    inputType = 'text'
+                //    style = {{marginTop: 10}}
+                //    disabled = {forAll}
+                ///>
+                //
+                //<div style = {{marginTop: 10}}>
+                //    <input checked = {forAll} type = 'checkbox' id = 'coupon-fol-all-cb' onClick = {(e) => setForAll(e.target.checked)} />
+                //    <label style = {{display: 'inline-block', verticalAlign: 'top', margin: '0px 6px'}} for = 'coupon-for-all-cb'>This coupon is valid for all</label>
+                //</div>
+                }
+                <p style = {{margin: '5px 0px'}}>
+                    {message}
+                </p>
                 <div style = {{marginTop: 20}}>
                     <button
                         type = 'submit' 
