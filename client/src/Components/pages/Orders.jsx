a760fbc5eeb912bfea05cb61779cc3a7f8dc856e
MacBook <alinauroze@hotmail.com>
2020-11-24 02:01:38 +0500

    feat: initialized order pafe

--- client/src/Components/pages/Orders.jsx
+++ client/src/Components/pages/Orders.jsx
@@ -1,6 +1,40 @@
 import React from 'react'
-import {View} from '../Basic/AppComponents'
+import {View, Text} from '../Basic/AppComponents'
+import Viewer from '../../utils/Viewer'
+import {request} from '../../utils/AppRequest'
 
 export default function () {
+
+    const [data, setData] = React.useState();
+    const [error, setError] = React.useState('');
+
+    if (! data) {
+
+        request({
+            route: 'orders',
+            method: 'GET',
+            credentials: 'include'
+        }).then(res => {
+            if (res.data) {
+                setData(res.data)
+            }
+            else {
+                setError('Some error occurred')
+            }
+        })
+
+    }
+
+    return (
+        <View>
+            {
+                data ? 
+                <Viewer 
+                    data = {data}
+                />
+                : error ? <Text>{error}</Text> : <Text>loading ...</Text>
+            }
+        </View>
+    )
 
 }
