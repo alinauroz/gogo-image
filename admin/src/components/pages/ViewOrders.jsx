94ffb316b74fbf13354890c3dda1ff43acfd812d
MacBook <alinauroze@hotmail.com>
2020-11-24 12:42:56 +0500

    feat: download order files added

--- admin/src/components/pages/ViewOrders.jsx
+++ admin/src/components/pages/ViewOrders.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import {request} from '../../utils/request'
 import Viewer from '../../utils/Viewer'
+import {api} from '../../data/api'
 
 export default function (props) {
 
     const [orders, setOrders] = React.useState();
     const [error, setError] = React.useState('');
+
+    const getOrderFile = async (e, data) => {
+
+        let res = await request({
+            route: 'orders/',
+            method: 'GET',
+            credentials: 'include',
+            params: data._id
+        })
+
+        if (res.status == 'success') {
+            window.open(api + 'zips/' + data.invoiceNo + '.zip', '_blank')
+        }
+        else alert('Error in downloading files');
+
+    }
 
     if (! orders) {
 
@@ -27,12 +44,15 @@ export default function (props) {
     }
 
     return (
-        <div className = 'card'>
+        <div className = 'box'>
             {
                 orders ?
                 <Viewer 
                     data = {orders}
                     hidden = {['submission', 'items', '_id', 'updatedAt']}
+                    actions = {[
+                        {onClick: getOrderFile, value: '⬇', className : 'btn btn-primary'}
+                    ]}
                 />
                 : error ? <>{error}</> : 'loading ...'
             }
