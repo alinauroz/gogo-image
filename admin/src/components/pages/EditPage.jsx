54235ff135e0d582578aa866c789be517dc3b81c
MacBook <alinauroze@hotmail.com>
2020-12-03 02:43:53 +0500

    feat: update page added

--- admin/src/components/pages/EditPage.jsx
+++ admin/src/components/pages/EditPage.jsx
@@ -2,15 +2,43 @@ import React from 'react'
 import Field from '../unit/Field'
 import CKEditor from 'react-ckeditor-component'
 import ReactHtmlParser from "react-html-parser";
+import {request} from '../../utils/request'
 
 export default function (props) {
 
     const [html, setHtml] = React.useState("");
+    const [title, setTitle] = React.useState("");
+    const [url, setURL] = React.useState("");
     const [toPreview, setToPreview] = React.useState();
 
     React.useEffect(() => {
 
     })
+
+    const updatePage = async () => {
+
+        let res;
+
+        console.log(url, props.base.isc.page.url,
+            title, props.base.isc.page.title,
+            html, props.base.isc.page.content)
+
+        if (props.base.isc.page)
+            res = await request({
+                route: 'pages/',
+                params: props.base.isc.page._id,
+                method: 'PUT',
+                credentials: 'include',
+                body: {
+                    url: url || props.base.isc.page.url,
+                    title: title || props.base.isc.page.title,
+                    content: html || props.base.isc.page.content
+                }
+            })
+        
+        console.log(res)
+
+    }
 
     return (
         <div class = 'card'>
@@ -19,7 +47,12 @@ export default function (props) {
                 placeholder = 'Title'
                 title = 'Title'
                 value = {props.base.isc.page ? props.base.isc.page.title : ''}
-
+                onChange = {(e) => {
+                    if (props.base.isc.page) {
+                        props.base.isc.page.title = e.target.value;
+                    }
+                    setTitle(e.target.value);
+                }}
             />
             <br />
             <span style = {{marginBottom: 10}}>
@@ -44,6 +77,12 @@ export default function (props) {
                 name = 'url'
                 placeholder = 'URL'
                 value = {props.base.isc.page ? props.base.isc.page.url : ''}
+                onChange = {(e) => {
+                    if (props.base.isc.page) {
+                        props.base.isc.page.url = e.target.value;
+                    }
+                    setURL(e.target.value);
+                }}
                 addon = '/'
             />
             <div style = {{marginTop: 15}}>
@@ -58,6 +97,7 @@ export default function (props) {
                     value = 'Update'
                     className = 'btn btn-primary'
                     style = {{marginLeft: 10}}
+                    onClick = {updatePage}
                 />
             </div>
         </div>
