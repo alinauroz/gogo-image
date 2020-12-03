4d044a34929587573e8e60b6c8c795a413630fc1
MacBook <alinauroze@hotmail.com>
2020-12-03 23:00:01 +0500

    feat: homepage populated with users count

--- admin/src/components/pages/Home.jsx
+++ admin/src/components/pages/Home.jsx
@@ -17,6 +17,18 @@ export default function (props) {
 
     }
 
+    if (Object.keys(users).length <= 0) {
+
+        request({
+            route: 'users/c/count',
+            method: 'GET',
+            credentials: 'include'
+        }).then(d => {
+            setUsers(d)
+        })
+
+    }
+
     const STATS = [
         {title: 'Orders', value: orders.total, desc: 'Total Orders'},
         {title: 'Completed', value: orders.completed, desc: 'Orders completed so far'},
@@ -25,12 +37,12 @@ export default function (props) {
         {title: 'Last Week', value: orders.lastWeek, desc: 'Orders in last Week'},
         {title: 'Last Month', value: orders.lastMonth, desc: 'Orders in last Month'},
         {title: 'Past Due', value: orders.pastDue, desc: 'Due date passed'},
-        {title: '', value: orders.uncompleted, desc: ''},
-        {title: '', value: orders.uncompleted, desc: ''},
-        {title: '', value: orders.uncompleted, desc: ''},
-        {title: '', value: orders.uncompleted, desc: ''},
-        {title: '', value: orders.uncompleted, desc: ''},
-        {title: '', value: orders.uncompleted, desc: ''},
+        {title: 'Total Users', value: users.total, desc: 'Users registered so far'},
+        {title: 'Confirmed Users', value: users.confirmed, desc: 'Confirmed their emails'},
+        {title: 'Unconfirmed Users', value: users.unconfirmed, desc: 'Didn\'t confirmed their emais '},
+        {title: 'Last 24 hrs', value: users.last24, desc: 'Buyers in last 24 hours'},
+        {title: 'Last Week', value: users.lastWeek, desc: 'Buyers in last Week'},
+        {title: 'Last Month', value: users.lastMonth, desc: 'Buyers in last Month'},
         {title: '', value: orders.uncompleted, desc: ''},
         {title: '', value: orders.uncompleted, desc: ''},
     ]
