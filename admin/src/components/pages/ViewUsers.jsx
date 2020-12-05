0cefed3ad39547b98e33c92bf765620c7e2cb4a9
MacBook <alinauroze@hotmail.com>
2020-12-06 01:53:42 +0500

    feat: pager added to view users

--- admin/src/components/pages/ViewUsers.jsx
+++ admin/src/components/pages/ViewUsers.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import {api} from '../../data/api'
 import Viewer from '../../utils/Viewer'
+import Pager from '../../utils/Pager'
 
 export default function (props) {
 
     const [data, setData] = React.useState();
+    const [pageSize, setPageSize] = React.useState(3);
+    const [startIndex, setStartIndex] = React.useState(0);
+
+    const setPage = (i_) => {
+        setStartIndex(--i_ * pageSize)
+    }
 
     const EditAction = (data) => {
         props.setBase(data);
@@ -28,14 +35,25 @@ export default function (props) {
             <br />
             {
                 data ?
+                <>
                 <Viewer 
-                    data = {data.data}
+                    data = {data.data.slice(startIndex, pageSize + startIndex)}
                     hidden = {['_id', 'emailConfirmToken']}
                     //actions = {[
                     //    {onClick: EditAction, value: 'Edit', className : 'btn btn-primary'},
                     //    {onClick: EditAction, value: 'Delete', className : 'btn btn-danger', break: true}
                     //]}
                 />
+                <div className = 'order-pager-container'>
+                    <span style = {{float: 'left'}}>
+                        Showing {startIndex + 1} to {(startIndex + 1) * pageSize < data.data.length ? (startIndex + 1) * pageSize: data.data.length} of {data.data.length} entries
+                    </span>
+                    <Pager 
+                        count = {Math.ceil(data.data.length / pageSize)}
+                        setPage = {setPage}
+                    />
+                </div>
+                </>
                 : "loading ..."
             }
         </div>
