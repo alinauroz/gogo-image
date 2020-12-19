d90f58101cfce9a774c7e9f468eb33520cb3dbaf
MacBook <alinauroze@hotmail.com>
2020-12-19 17:58:36 +0500

    style: blog styles cahnged

--- client/src/Components/pages/Blogs.jsx
+++ client/src/Components/pages/Blogs.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import {request} from '../../utils/AppRequest'
 import {api} from '../../data/api'
 import ReactHtmlParser from "react-html-parser";
+import {Text} from '../Basic/AppComponents'
 import { Link } from 'react-router-dom'
 
 export default function (props) {
 
     const [blogs, setBlogs] = React.useState();
+
+    const getDate = date => {
+        let _date = new Date(date);
+        return (
+            <span style = {{padding: 3, background: '#ddd', marginRight: 5}}>{
+            _date.getDate() + '/' +
+            _date.getMonth() + '/' +
+            _date.getFullYear()}
+            </span>
+        )
+    }
 
     React.useEffect(() => {
         document.title = 'Blogs - ' + (props.info ? props.info.name: '');
@@ -22,43 +34,26 @@ export default function (props) {
 
     let html = [];
     let blogList = [];
-
-    if (blogs && blogs.length >= 0) {
-        html.push(
-            <Link to = {'blogpost/' + blogs[0]._id} style = {{color: '#333', textDecoration: 'none'}}>
-            <div style = {{color: '#333', textDecoration: 'none'}}>
-                <div style = {{width: '100%', height: 350, backgroundImage: blogs[0].image? `url(${blogs[0].image})`: 'url(https://icsb.org/wp-content/uploads/membership-profile-uploads/profile_image_placeholder.png)', backgroundSize: 'cover', backgroundPosition: '50%'}}>
-
-                </div>
-                <div className = 'main-blog-info-container'>
-                    <p style = {{fontWeight: 'bold', marginTop: 10}}>{blogs[0].title}</p>
-                    <p>{ReactHtmlParser(ReactHtmlParser( blogs[0].content.substr(0, 500)))}</p>
-                </div>
-            </div>
-            </Link>
-        )
-
-        blogs.map((blog, index) => {
-            if (index !== 0) {
-                blogList.push(
-                    <Link to = {'blogpost/' + blog._id} style = {{color: '#333', textDecoration: 'none'}}>
-                    <div className = 'blog-list-item-container' style = {{marginTop: 15, verticalAlign: 'top', display: 'inline-block', width: 'calc(33.3333333333% - 10px)', marginRight: index % 3 !== 0 ? 15: 0}}>
-                        <div className = 'blog-list-image' style = {{width: '100%', height: 350, backgroundImage: blog.image? `url(${blog.image})`: 'url(https://icsb.org/wp-content/uploads/membership-profile-uploads/profile_image_placeholder.png)', backgroundPosition: '50%', backgroundSize: 'cover'}}>
-
-                        </div>
-                        <p style = {{fontWeight: 'bold', marginTop: 10}}>{blog.title}</p>
-                        <p>{ReactHtmlParser(ReactHtmlParser( blog.content.substr(0, 100)))}</p>
+    blogs &&
+    blogs.map((blog, index) => {
+        if (index !== -1) {
+            blogList.push(
+                <Link to = {'blogpost/' + blog._id} style = {{color: '#333', textDecoration: 'none', textTransform: 'capitalize', marginTop: 10}}>
+                <div className = 'blog-list-item-container' style = {{fontSize: 13, marginTop: 10}}>
+                    <p style = {{marginTop: 10, marginBottom: 10}}>{getDate(Date.now())}{blog.title}</p>
+                    <p style={{marginBottom: 7}}><b>Category: </b>{blog.category}</p>
+                    <div className = 'blog-list-image' style = {{display: 'inline-block', width: 'calc(50% - 20px)', height: 350, backgroundImage: blog.image? `url(${blog.image})`: 'url(https://icsb.org/wp-content/uploads/membership-profile-uploads/profile_image_placeholder.png)', backgroundPosition: '50%', backgroundSize: 'cover'}}>
                     </div>
-                    </Link>
-                )
-            }
-        })
+                    
+                    <p style={{display: 'inline-block', width: 'calc(50% - 10px)', marginLeft: 10, verticalAlign: 'top'}}>{ReactHtmlParser(ReactHtmlParser( blog.content.substr(0, 600)))}<br /><input type='button' style = {{width: 100}} class='action-button' value = 'Read Now' /></p>
+                    
+                </div>
+                </Link>
+            )
+        }
+    })
 
-    }
-    else if (blogs && blogs.length == 0) {
-        return <>No Blog</>
-    }
-
-    return <div className = 'blog-container' style = {{}}>{html}<div style = {{margin:'20px 0px'}}>{blogList}</div></div>
+    return (<div className = 'blog-container navbox' style = {{}}><Text className = 'navbox-title-shadow'>Blogs</Text>
+    <Text className = 'navbox-title'>Blogs</Text><div style = {{margin:'20px 0px'}}>{blogList}</div></div>)
 
 }
