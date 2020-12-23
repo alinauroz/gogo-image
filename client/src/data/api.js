4eef921939f31b39d25c84da16bfc9ef0691cea7
MacBook <alinauroze@hotmail.com>
2020-12-23 15:59:35 +0500

    api changed

--- client/src/data/api.js
+++ client/src/data/api.js
@@ -1,6 +1,6 @@
 export const apis = {
     'dev': 'http://localhost:5000/',
-    'prod': 'http://picsfies:5000/'
+    'prod': 'http://picsfies.com:5000/'
 }
 
 export const MODE = /localhost/.test(window.location.href) ? 'dev' : 'prod';
