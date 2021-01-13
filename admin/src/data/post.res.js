d8dbe4817458b92b17b6f3925373068b5d7a376b
MacBook <alinauroze@hotmail.com>
2021-01-14 01:42:10 +0500

    category resourse added

---
+++ admin/src/data/post.res.js
@@ -0,0 +1,12 @@
+const categories = [
+    ['Borders', 'BR'],
+    ['Condensation', 'CN'],
+    ['DUST', 'DT'],
+    ['FIRE', 'FE'],
+    ['Floral', 'FL'],
+    ['FROST', 'FT'],
+    ['GLASS', 'GS'],
+    ['Glitters', 'GR'],
+    ['Lightning', 'LG'],
+    ['Rain', 'RN']
+]
