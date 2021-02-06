f0bf0b60889b05990a2aad255716215d7d9bcbcb
MacBook <alinauroze@hotmail.com>
2021-02-06 19:58:09 +0500

    feat: send message

--- admin/src/components/pages/Dispute.jsx
+++ admin/src/components/pages/Dispute.jsx
@@ -25,6 +25,12 @@ const Dispute = (props) => {
         })
         setData({... data});
         setCurrentContent('');
+
+        request({
+            method: 'PUT',
+            route: 'contact/' + id,
+            body: data
+        })
     }
 
     const getData = async () => {
